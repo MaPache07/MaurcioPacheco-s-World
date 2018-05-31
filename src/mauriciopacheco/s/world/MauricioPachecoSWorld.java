49639b715160552f08491e3a9160f10c7ce15621
MaPache <00170517@uca.edu.sv>
2018-05-31 05:54:43 -0600

    Se creo el proyecto incluida la clase Main

---
+++ src/mauriciopacheco/s/world/MauricioPachecoSWorld.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mauriciopacheco.s.world;
+
+/**
+ *
+ * @author MaPache
+ */
+public class MauricioPachecoSWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
